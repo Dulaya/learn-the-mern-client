57f0268598afc68ec23992c0bcf82de3818e48b8
dulaya <dulaya.saennok@gmail.com>
2021-03-03 17:18:29 -0800

    add side bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
+import { Container, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
 
 const containerStyle = {
   background: '#1e212d',
-  height: '100vh',
-  margin: '0',
-  overFlow: 'hidden',
-  position: 'fixed',
+  minHeight: '100vh',
   textAlign: 'center',
   width: '100vw'
 }
 
 const neonStyle = {
   color: '#fff',
-  lineHeight: '50vh',
   fontSize: '5rem',
   margin: '0',
   textShadow: `
@@ -33,7 +30,29 @@ const iconStyle = {
   fontSize: '5rem',
 }
 
+const accordionStyle = {
+  margin: '10px',
+  maxWidth: '250px',
+}
+
+const accordionHeaderStyle = {
+  background: '#03506f',
+  color: 'white',
+}
+
+const buttonStyle = {
+  margin: '5px 0',
+  width: '100%',
+}
+
 const App = () => {
+
+  const [info, setInfo] = useState('');
+
+  const frontEndArray = ['JavaScript', 'React', 'React Router DOM', 'Context API', 'API'];
+  const backEndArray = ['Node.js', 'Express.js', 'MongoDB'];
+  const authenticationArray  = ['JSON Web Token'];
+  const stylingArray  = ['Material UI', 'React Bootstrap'];
 
   //Open New Tab
   const openInNewTab = (url) => {
@@ -46,6 +65,74 @@ const App = () => {
       <div style={neonStyle}>
         Learn the MERN
       </div>
+
+      <Row>
+        <Col style={{ maxWidth: '300px' }}>
+          <Accordion style={accordionStyle} defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="0">
+                Front End
+          </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  {
+                    frontEndArray.map(concept =>
+                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                    )
+                  }
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="1">
+                Back End
+           </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  {
+                    backEndArray.map(concept =>
+                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                    )
+                  }
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="2">
+                Authentication
+           </Accordion.Toggle>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  {
+                    authenticationArray.map(concept =>
+                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                    )
+                  }
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="3">
+                Styling
+          </Accordion.Toggle>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>
+                  {
+                    stylingArray.map(concept =>
+                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                    )
+                  }
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Col>
+
+        <Col style={{ color: 'white', maxWidth: '75vw', border: '1px solid' }}>
+          <p style={{ fontSize: '2rem', }}>Some info about: {info}</p>
+        </Col>
+      </Row>
+
       <AiFillGithub style={iconStyle} onClick={() => openInNewTab('https://github.com/dulaya/learn-the-mern-client')} />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//Need this import for Bootstrap to work
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <>
     <App />
