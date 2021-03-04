88af1f9175e365a3bd965d6c010f8b09dafef546
dulaya <dulaya.saennok@gmail.com>
2021-03-04 02:34:31 -0800

    added React Router Dom for handling routes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { AiFillGithub } from 'react-icons/ai';
-import { Container, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
+import { Navbar, Nav, Row, Col, Accordion, Card, Button } from 'react-bootstrap';
 
-const containerStyle = {
-  background: '#1e212d',
-  minHeight: '100vh',
-  textAlign: 'center',
-  width: '100vw'
-}
+import {
+  containerStyle,
+  neonStyle,
+  iconStyle,
+  accordionStyle,
+  accordionHeaderStyle,
+  buttonStyle,
+} from './Styles.js';
 
-const neonStyle = {
-  color: '#fff',
-  fontSize: '5rem',
-  margin: '0',
-  textShadow: `
-    0 0 5px #fff,
-    0 0 10px #fff,
-    0 0 20px #fff,
-    0 0 40px #0ff,
-    0 0 80px #0ff,
-    0 0 90px #0ff,
-    0 0 100px #0ff,
-    0 0 150px #0ff`,
-}
-
-const iconStyle = {
-  color: 'white',
-  cursor: 'pointer',
-  fontSize: '5rem',
-}
-
-const accordionStyle = {
-  margin: '10px',
-  maxWidth: '250px',
-}
-
-const accordionHeaderStyle = {
-  background: '#03506f',
-  color: 'white',
-}
-
-const buttonStyle = {
-  margin: '5px 0',
-  width: '100%',
-}
 
 const App = () => {
 
@@ -51,8 +19,8 @@ const App = () => {
 
   const frontEndArray = ['JavaScript', 'React', 'React Router DOM', 'Context API', 'API'];
   const backEndArray = ['Node.js', 'Express.js', 'MongoDB'];
-  const authenticationArray  = ['JSON Web Token'];
-  const stylingArray  = ['Material UI', 'React Bootstrap'];
+  const authenticationArray = ['JSON Web Token'];
+  const stylingArray = ['Material UI', 'React Bootstrap'];
 
   //Open New Tab
   const openInNewTab = (url) => {
@@ -61,80 +29,102 @@ const App = () => {
   }
 
   return (
-    <div style={containerStyle}>
-      <div style={neonStyle}>
-        Learn the MERN
+    <Router>
+      <div style={containerStyle}>
+
+        <Navbar bg="light" expand="lg">
+          <Link style={{ margin: '5px' }} to='/'>Home</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link to='/forum'>Forum</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+
+        <div style={neonStyle}>
+          Learn the MERN
       </div>
 
-      <Row>
-        <Col style={{ maxWidth: '300px' }}>
-          <Accordion style={accordionStyle} defaultActiveKey="0">
-            <Card>
-              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="0">
-                Front End
+        <Route exact path='/'>
+          <Row>
+            <Col style={{ maxWidth: '300px' }}>
+              <Accordion style={accordionStyle} defaultActiveKey="0">
+                <Card>
+                  <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="0">
+                    Front End
           </Accordion.Toggle>
-              <Accordion.Collapse eventKey="0">
-                <Card.Body>
-                  {
-                    frontEndArray.map(concept =>
-                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
-                    )
-                  }
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="1">
-                Back End
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                      {
+                        frontEndArray.map(concept =>
+                          <Button key={concept} variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                        )
+                      }
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="1">
+                    Back End
            </Accordion.Toggle>
-              <Accordion.Collapse eventKey="1">
-                <Card.Body>
-                  {
-                    backEndArray.map(concept =>
-                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
-                    )
-                  }
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="2">
-                Authentication
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                      {
+                        backEndArray.map(concept =>
+                          <Button key={concept} variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                        )
+                      }
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="2">
+                    Authentication
            </Accordion.Toggle>
-              <Accordion.Collapse eventKey="2">
-                <Card.Body>
-                  {
-                    authenticationArray.map(concept =>
-                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
-                    )
-                  }
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-            <Card>
-              <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="3">
-                Styling
+                  <Accordion.Collapse eventKey="2">
+                    <Card.Body>
+                      {
+                        authenticationArray.map(concept =>
+                          <Button key={concept} variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                        )
+                      }
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>
+                  <Accordion.Toggle style={accordionHeaderStyle} as={Card.Header} eventKey="3">
+                    Styling
           </Accordion.Toggle>
-              <Accordion.Collapse eventKey="3">
-                <Card.Body>
-                  {
-                    stylingArray.map(concept =>
-                      <Button variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
-                    )
-                  }
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          </Accordion>
-        </Col>
+                  <Accordion.Collapse eventKey="3">
+                    <Card.Body>
+                      {
+                        stylingArray.map(concept =>
+                          <Button key={concept} variant='info' style={buttonStyle} onClick={() => setInfo(concept)}>{concept}</Button>
+                        )
+                      }
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Col>
 
-        <Col style={{ color: 'white', maxWidth: '75vw', border: '1px solid' }}>
-          <p style={{ fontSize: '2rem', }}>Some info about: {info}</p>
-        </Col>
-      </Row>
+            <Col style={{ color: 'white', maxWidth: '75vw', border: '1px solid' }}>
+              <p style={{ fontSize: '2rem', }}>Some info about: {info}</p>
+            </Col>
+          </Row>
+        </Route>
 
-      <AiFillGithub style={iconStyle} onClick={() => openInNewTab('https://github.com/dulaya/learn-the-mern-client')} />
-    </div>
+        <Route exact path='/forum'>
+          <h1 style={{ color: 'white' }}>Forum</h1>
+          <h1 style={{ color: 'white' }}>Nothing to show here yet. Only show route handling.</h1>
+        </Route>
+
+        <AiFillGithub style={iconStyle} onClick={() => openInNewTab('https://github.com/dulaya/learn-the-mern-client')} />
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/Styles.js
@@ -0,0 +1,42 @@
+export const containerStyle = {
+    background: '#1e212d',
+    minHeight: '100vh',
+    textAlign: 'center',
+    width: '100vw'
+  }
+
+export const neonStyle = {
+    color: '#fff',
+    fontSize: '5rem',
+    margin: '0',
+    textShadow: `
+      0 0 5px #fff,
+      0 0 10px #fff,
+      0 0 20px #fff,
+      0 0 40px #0ff,
+      0 0 80px #0ff,
+      0 0 90px #0ff,
+      0 0 100px #0ff,
+      0 0 150px #0ff`,
+  }
+  
+export const iconStyle = {
+    color: 'white',
+    cursor: 'pointer',
+    fontSize: '5rem',
+  }
+  
+export const accordionStyle = {
+    margin: '10px',
+    maxWidth: '250px',
+  }
+  
+export const accordionHeaderStyle = {
+    background: '#03506f',
+    color: 'white',
+  }
+  
+export const buttonStyle = {
+    margin: '5px 0',
+    width: '100%',
+  }
